340c80887758f692cb49900540e09d32c06a1ee4
Ishmael Belghazi <ishar667@gmail.com>
2015-06-10 18:24:27 -0400

    Fixed ac bug

--- radio/elisp/radio-ac.el
+++ radio/elisp/radio-ac.el
@@ -8,28 +8,28 @@
 
 (defun radio-text-modes-setup ()
   "Radio Emacs auto-complete text modes setup"
-  (setq 'ac-sources '(ac-source-files-in-current-dir
-		      ac-source-filename
-		      ac-source-dictionary
-		      ac-source-words-in-buffer
-		      ac-source-yasnippet)))
+  (setq ac-sources '(ac-source-files-in-current-dir
+		     ac-source-filename
+		     ac-source-dictionary
+		     ac-source-words-in-buffer
+		     ac-source-yasnippet)))
 
 (defun radio-ac-tex-modes-setup ()
   "Radio Emacs auto-complete tex modes setup"
-  (setq 'ac-sources '(ac-source-files-in-current-dir
-		      ac-source-filename
-		      ac-source-dictionary
-		      ac-source-words-in-buffer
-		      ac-source-yasnippet
-		      )))
+  (setq ac-sources '(ac-source-files-in-current-dir
+		     ac-source-filename
+		     ac-source-dictionary
+		     ac-source-words-in-buffer
+		     ac-source-yasnippet
+		     )))
 
 (defun radio-ac-prog-modes-setup ()
   "Radio Emacs auto-complete prog modes setup"
-  (setq 'ac-sources '(ac-source-dictionary
-		      ac-source-filename
-		      ac-source-files-in-current-dir
-		      ac-source-yasnippet
-		      )))
+  (setq ac-sources '(ac-source-dictionary
+		     ac-source-filename
+		     ac-source-files-in-current-dir
+		     ac-source-yasnippet
+		     )))
 
 
 
