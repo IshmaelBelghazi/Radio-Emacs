794f032992d459c4931303603ababda2ebe1a132
Ishmael Belghazi <ishar667@gmail.com>
2015-06-16 17:39:33 -0400

    update projectile

--- radio/elisp/radio-projectile.el
+++ radio/elisp/radio-projectile.el
@@ -3,6 +3,7 @@
 ;; Enabling helm projectile
 (require 'helm-projectile)
 (helm-projectile-on)
-
+(setq projectile-completion-system 'helm)
+(setq projectile-indexing-method 'alien)
 
 (provide 'radio-projectile)
