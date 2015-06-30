184ecf36e9d721ab3ba53fd5a4ef9dc30588b956
Ishmael Belghazi <ishar667@gmail.com>
2015-06-30 13:19:54 -0400

    cedet fix

--- init.el
+++ init.el
@@ -4,8 +4,10 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 ;; Loading CEDET
 ;; Unloading Builtin cedet
-;;(setq load-path (remove-if (lambda (x) (string-match-p "cedet" x)) load-path))
-(load-file (concat user-emacs-directory "/el-get/cedet/cedet-remove-builtin.el"))
+(require 'cl)
+
+(setq load-path (remove-if (lambda (x) (string-match-p "cedet" x)) load-path))
+;;(load-file (concat user-emacs-directory "/el-get/cedet/cedet-remove-builtin.el"))
 ;;(cedet-remove-builtin)
 ;; Loading devel CEDET
 (load-file (concat user-emacs-directory "/el-get/cedet/cedet-devel-load.el"))
@@ -16,7 +18,7 @@
 (global-semantic-idle-scheduler-mode 1)
 (global-semantic-idle-summary-mode 1)
 ;;(global-semantic-idle-breadcrumbs-mode 1)
-(semanticdb-enable-gnu-global-databases)
+(semanticdb-enable-gnu-global-databases t)
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -110,8 +112,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-bar-mode nil)
- '(semantic-matlab-dependency-system-include-path nil))
+ '(package-selected-packages (quote (srefactor))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
