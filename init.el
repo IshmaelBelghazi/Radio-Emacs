446686f7fc95f4781481f04c5e5e636994028712
Ishmael Belghazi <ishmael.belghazi@gmail.com>
2015-07-01 12:21:52 -0400

    added org mode

--- init.el
+++ init.el
@@ -3,20 +3,20 @@
 ;; El-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 ;; Loading CEDET
-;; Unloading Builtin cedet
-;;(setq load-path (remove-if (lambda (x) (string-match-p "cedet" x)) load-path))
-(load-file (concat user-emacs-directory "/el-get/cedet/cedet-remove-builtin.el"))
-;;(cedet-remove-builtin)
-;; Loading devel CEDET
-(load-file (concat user-emacs-directory "/el-get/cedet/cedet-devel-load.el"))
-(load-file (concat user-emacs-directory "/el-get/cedet/contrib/cedet-contrib-load.el"))
-(require 'semantic)
-(require 'ede)
-(global-semanticdb-minor-mode 1)
-(global-semantic-idle-scheduler-mode 1)
-(global-semantic-idle-summary-mode 1)
-;;(global-semantic-idle-breadcrumbs-mode 1)
-(semanticdb-enable-gnu-global-databases)
+;; ;; Unloading Builtin cedet
+;; ;;(setq load-path (remove-if (lambda (x) (string-match-p "cedet" x)) load-path))
+;; (load-file (concat user-emacs-directory "/el-get/cedet/cedet-remove-builtin.el"))
+;; ;;(cedet-remove-builtin)
+;; ;; Loading devel CEDET
+;; (load-file (concat user-emacs-directory "/el-get/cedet/cedet-devel-load.el"))
+;; (load-file (concat user-emacs-directory "/el-get/cedet/contrib/cedet-contrib-load.el"))
+;; (require 'semantic)
+;; (require 'ede)
+;; (global-semanticdb-minor-mode 1)
+;; (global-semantic-idle-scheduler-mode 1)
+;; (global-semantic-idle-summary-mode 1)
+;; ;;(global-semantic-idle-breadcrumbs-mode 1)
+;; (semanticdb-enable-gnu-global-databases)
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -69,6 +69,8 @@
 ;; ** Edit
 ;; *** Common
 ;; *** Mode specific
+;; **** Org Mode
+(require 'radio-org)
 ;; **** Markdown
 (require 'radio-markdown)
 ;; **** Restructured text
@@ -103,18 +105,4 @@
 ;; * Loading Helper functions
 (require 'helper-fun)
 (provide 'init)
-;;; init.el ends here
-(put 'erase-buffer 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(scroll-bar-mode nil)
- '(semantic-matlab-dependency-system-include-path nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
